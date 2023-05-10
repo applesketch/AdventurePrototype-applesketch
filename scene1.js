62265815678919592b53cc09e007d130edf23372
applesketch <applezsketch@gmail.com>
2023-05-09 20:59:35 -0700

    organizing

---
+++ scene1.js
@@ -0,0 +1,68 @@
+class Scene1 extends AdventureScene {
+    constructor() {
+        super("scene1", "Classroom");
+    }
+
+    preload() {
+        this.load.path = "./img/";
+        this.load.image("studio", "studio.png");
+        this.load.audio("boom", "boom.mp3");
+    }
+
+    onEnter() {
+
+        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => this.showMessage("Metal, bent."))
+            .on('pointerdown', () => {
+                this.showMessage("No touching!");
+                this.tweens.add({
+                    targets: clip,
+                    x: '+=' + this.s,
+                    repeat: 2,
+                    yoyo: true,
+                    ease: 'Sine.inOut',
+                    duration: 100
+                });
+            });
+
+        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("It's a nice key.")
+            })
+            .on('pointerdown', () => {
+                this.showMessage("You pick up the key.");
+                this.gainItem('key');
+                this.tweens.add({
+                    targets: key,
+                    y: `-=${2 * this.s}`,
+                    alpha: { from: 1, to: 0 },
+                    duration: 500,
+                    onComplete: () => key.destroy()
+                });
+            })
+
+        let door = this.add.text(this.w * 0.01, this.w * 0.3, "🚪 locked door")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                if (this.hasItem("key")) {
+                    this.showMessage("You've got the key for this door.");
+                } else {
+                    this.showMessage("It's locked. Can you find a key?");
+                }
+            })
+            .on('pointerdown', () => {
+                if (this.hasItem("key")) {
+                    this.loseItem("key");
+                    this.showMessage("*squeak*");
+                    door.setText("🚪 unlocked door");
+                    this.gotoScene('scene2');
+                }
+            })
+
+    }
+}
---
+++ scene3.js
@@ -0,0 +1,30 @@
+class Scene3 extends AdventureScene {
+    constructor() {
+        super("scene3", "The second room has a long name (it truly does).");
+    }
+    onEnter() {
+        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("You've got no other choice, really.");
+            })
+            .on('pointerdown', () => {
+                this.gotoScene('scene1');
+            });
+
+        let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage('*giggles*');
+                this.tweens.add({
+                    targets: finish,
+                    x: this.s + (this.h - 2 * this.s) * Math.random(),
+                    y: this.s + (this.h - 2 * this.s) * Math.random(),
+                    ease: 'Sine.inOut',
+                    duration: 500
+                });
+            })
+            .on('pointerdown', () => this.gotoScene('outro'));
+    }
+}
