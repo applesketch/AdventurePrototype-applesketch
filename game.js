8faae0e70b57591a02f5fd74a0cbea02f5f80de8
applesketch <applezsketch@gmail.com>
2023-05-11 10:36:39 -0700

    new music :music:

--- game.js
+++ game.js
@@ -135,13 +135,63 @@ class Intro extends Phaser.Scene {
     constructor() {
         super('intro')
     }
+    preload() {
+        this.load.path = "./img/";
+        this.load.image("studio", "studio.png");
+        this.load.audio("boom", "boom.mp3");
+    }
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
-        this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
-        this.input.on('pointerdown', () => {
-            this.cameras.main.fade(1000, 0,0,0);
+        let studio = this.add.sprite(900, 500, "studio")
+        this.time.delayedCall(2000, () => {
+            this.cameras.main.fadeOut(2000, 255,255,255);
             this.time.delayedCall(1000, () => this.scene.start('scene1'));
+            this.sound.add("boom").play();
         });
+    }
+}
+
+class Menu extends Phaser.Scene {
+    constructor() {
+        super('menu');
+    }
+
+    preload() {
+        this.load.path = "./img/";
+        this.load.image("title", "title.png");
+        this.load.audio("music1", "music1.mp3");
+    }
+
+    create() {
+        let title = this.add.sprite(300, 50, "title");
+        let box = this.add.text(70, 140,
+`PLAY
+
+
+SETTINGS
+
+
+QUIT`
+        );
+
+        let circle1 = this.add.circle(50, 150, 10, 0xFF0000);
+        let circle2 = this.add.circle(50, 190, 10, 0xFF0000);
+        let circle3 = this.add.circle(50, 230, 10, 0xFF0000);
+        
+        let cat = this.add.sprite(300, 300, "cat");
+        this.add.tween({
+            targets: cat,
+            scale: {from: 0, to: 1},
+            duration: 1000
+        });
+       
+
+        this.input.on('pointerdown', () => {
+            this.time.delayedCall(2000, () => {
+                this.cameras.main.fadeOut(2000, 255,255,255);
+            }); this.scene.start('scene1')
+            });
+        this.sound.add("siu").play();
+        
     }
 }
 
@@ -164,6 +214,6 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Intro, Scene1, Scene2, Outro],
+    scene: [Intro, Menu, Scene1, Scene2, Outro],
     title: "Escape",
 });
